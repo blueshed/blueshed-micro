5ecccdd2b95e4b90ba54d604b44a68c0c512dd5e
blueshed <pete@blueshed.co.uk>
2016-05-12 01:05:01 +0100

    make typing safe for pre python 3.5

--- blueshed/micro/__init__.py
+++ blueshed/micro/__init__.py
@@ -1,3 +1,9 @@
-from typing import TypeVar
 
-__builtins__['micro_context'] = TypeVar('micro_context')
+try:
+    from typing import TypeVar
+
+    __builtins__['micro_context'] = TypeVar('micro_context')
+
+except ImportError as ex:
+    ''' requires python 3.5 '''
+    pass
